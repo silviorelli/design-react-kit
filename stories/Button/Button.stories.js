63619e0b070a01e8845a05d60676f379736f8246
Francesco Zaia <francesco.zaia@gmail.com>
2018-06-28 16:05:48 +0200

    Aggiornata dipendenza Bootstrap Italia, semplificata Story del componente Button

--- stories/Button/Button.stories.js
+++ stories/Button/Button.stories.js
@@ -9,72 +9,44 @@ import { Button } from "../../src";
 
 import Esempi from "./Esempi.md";
 import Dimensioni from "./Dimensioni.md";
-import StatoAttivo from "./StatoAttivo.md";
-import StatoDisabilitato from "./StatoDisabilitato.md";
 import EsempiInterattivi from "./EsempiInterattivi.md";
 
 const stories = storiesOf("Componenti/Button", module);
-stories.addDecorator(checkA11y);
+stories
+    .addDecorator(checkA11y)
+    .addDecorator(withKnobs);
 
 const EsempiComponent = () => (
-    <div>
-        <Button color="italia" className="mr-1">
-            Italia
-        </Button>
-        <Button color="primary" className="mr-1">
-            Primary
-        </Button>
-        <Button color="secondary" className="mr-1">
-            Secondary
-        </Button>
-        <Button color="tertiary" className="mr-1">
-            Tertiary
-        </Button>
-        <Button color="quaternary" className="mr-1">
-            Quaternary
-        </Button>
-        <Button color="success" className="mr-1">
-            Success
-        </Button>
-        <Button color="info" className="mr-1">
-            Info
-        </Button>
-        <Button color="danger" className="mr-1">
-            Danger
-        </Button>
-        <Button color="warning" className="mr-1">
-            Warning
-        </Button>
-        <Button color="link" className="mr-1">
-            Link
-        </Button>
-    </div>
+    <section>
+        <div className="my-1">
+            <Button color="primary" className="mx-1">primary</Button>
+            <Button color="primary" className="mx-1" active>primary active</Button>
+            <Button color="primary" className="mx-1" outline>primary outline</Button>
+            <Button color="primary" className="mx-1" disabled>primary disabled</Button>
+        </div>
+        <div className="my-1">
+            <Button color="secondary" className="mx-1">secondary</Button>
+            <Button color="secondary" className="mx-1" active>secondary active</Button>
+            <Button color="secondary" className="mx-1" outline>secondary outline</Button>
+            <Button color="secondary" className="mx-1" disabled>secondary disabled</Button>
+        </div>
+    </section>
 );
 stories.add("Esempi", withDocs(Esempi, withInfo()(EsempiComponent)));
 
 const DimensioniComponent = () => (
     <section>
-        <div>
-            <Button color="primary" size="lg">
-                Large Button
-            </Button>{" "}
-            <Button color="secondary" size="lg">
-                Large Button
-            </Button>
-            <div className="mt-3" />
-            <Button color="primary" size="sm">
-                Small Button
-            </Button>{" "}
-            <Button color="secondary" size="sm">
-                Small Button
-            </Button>
-            <div className="mt-3" />
-            <Button color="primary" block>
-                Block level button
-            </Button>
-            <Button color="secondary" block>
-                Block level button
-            </Button>
+        <div className="my-1">
+            <Button color="primary" size="lg">Large Button</Button>
+        </div>
+        <div className="my-1">
+            <Button color="primary" size="sm">Small Button</Button>
+        </div>
+        <div className="my-1">
+            <Button color="primary" size="xs">Mini Button</Button>
+        </div>
+        <div className="my-1">
+            <Button color="primary" block>Block level button</Button>
         </div>
     </section>
 );
@@ -83,117 +55,27 @@ stories.add(
     withDocs(Dimensioni, withInfo()(DimensioniComponent))
 );
 
-const StatoAttivoComponent = () => (
-    <div>
-        <Button color="italia" active className="mr-1">
-            Italia
-        </Button>
-        <Button color="primary" active className="mr-1">
-            Primary
-        </Button>
-        <Button color="secondary" active className="mr-1">
-            Secondary
-        </Button>
-        <Button color="tertiary" active className="mr-1">
-            Tertiary
-        </Button>
-        <Button color="quaternary" active className="mr-1">
-            Quaternary
-        </Button>
-        <Button color="success" active className="mr-1">
-            Success
-        </Button>
-        <Button color="info" active className="mr-1">
-            Info
-        </Button>
-        <Button color="danger" active className="mr-1">
-            Danger
-        </Button>
-        <Button color="warning" active className="mr-1">
-            Warning
-        </Button>
-        <Button color="link" active className="mr-1">
-            Link
-        </Button>
-    </div>
-);
-stories.add(
-    "Stato Attivo",
-    withDocs(StatoAttivo, withInfo()(StatoAttivoComponent))
-);
-
-const StatoDisabilitatoComponent = () => (
-    <div>
-        <Button color="italia" disabled className="mr-1">
-            Italia
-        </Button>
-        <Button color="primary" disabled className="mr-1">
-            Primary
-        </Button>
-        <Button color="secondary" disabled className="mr-1">
-            Secondary
-        </Button>
-        <Button color="tertiary" disabled className="mr-1">
-            Tertiary
-        </Button>
-        <Button color="quaternary" disabled className="mr-1">
-            Quaternary
-        </Button>
-        <Button color="success" disabled className="mr-1">
-            Success
-        </Button>
-        <Button color="info" disabled className="mr-1">
-            Info
-        </Button>
-        <Button color="danger" disabled className="mr-1">
-            Danger
-        </Button>
-        <Button color="warning" disabled className="mr-1">
-            Warning
-        </Button>
-        <Button color="link" disabled className="mr-1">
-            Link
-        </Button>
-    </div>
-);
-stories.add(
-    "Stato Disabilitato",
-    withDocs(StatoDisabilitato, withInfo()(StatoDisabilitatoComponent))
-);
-
-const knobsStories = storiesOf("Componenti/Button", module);
-knobsStories.addDecorator(checkA11y);
-knobsStories.addDecorator(withKnobs);
-
 const EsempiInterattiviComponent = () => {
     const colors = [
-        "italia",
         "primary",
-        "secondary",
-        "tertiary",
-        "quaternary",
-        "success",
-        "info",
-        "danger",
-        "warning",
-        "link"
+        "secondary"
     ];
-    const dimensions = ["sm", "", "lg"];
+    const dimensions = ["xs", "sm", "md", "lg"];
     const color = select("Variazioni", colors, colors[0]);
-    const dimension = select("Dimensioni", dimensions, dimensions[0]);
+    const outline = boolean("Outline", false);
+    const dimension = select("Dimensioni", dimensions, dimensions[2]);
     const block = boolean("Block", false);
-    const active = boolean("Attivo", true);
+    const active = boolean("Attivo", false);
     const disabled = boolean("Disabilitato", false);
     const label = text("Label", "");
 
     return (
-        <Button color={color} block={block} active={active} disabled={disabled}>
-            {label} {color} {dimension} {block ? "block" : ""}{" "}
-            {active ? "attivo" : ""} {disabled ? "disabilitato" : ""}
+        <Button color={color} block={block} active={active} disabled={disabled} outline={outline} size={dimension}>
+            {label ? label : "Bottone di Esempio"}
         </Button>
     );
 };
-knobsStories.add(
+stories.add(
     "Esempi interattivi",
     withDocs(EsempiInterattivi, withInfo()(EsempiInterattiviComponent))
 );
